0993608f4e9bbaa5fc4e75c7e99a6b0e136e7524
Sayem Hossen <sayemhossen873@gmail.com>
2025-02-17 14:02:26 +0600

    gg

---
+++ vendor/maximebf/debugbar/src/DebugBar/Resources/vendor/highlightjs/highlight.pack.d.ts
@@ -0,0 +1,8 @@
+export = hljs;
+/*!
+  Highlight.js v11.9.0 (git: b7ec4bfafc)
+  (c) 2006-2024 undefined and other contributors
+  License: BSD-3-Clause
+  php, javascript, sql, shell, css, plaintext, xml
+ */
+declare var hljs: any;
